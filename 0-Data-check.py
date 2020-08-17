6fee5beaa84e5c83297aecbb28934f485c403a1e
johnwoodill <johnwoodill@gmail.com>
2020-08-17 10:49:10 -0700

    data check

--- 0-Data-check.py
+++ 0-Data-check.py
@@ -1,10 +1,40 @@
 import pandas as pd
 import numpy as np
+import glob
+import xarray as xr
+import os
+import multiprocessing
+from dask import delayed, compute
+from distributed import Client
+import gdal
+import os
+
+
+### Convert to tif
+# --------------------------------------
+
+import gdal, os
 
 FDIR = '/data2/NASA_Nighttime_Lights/'
 
+file = 'h111213v/h11v12/VNP46A2.A2019100.h11v12.001.boat.hdf'
 
+file_loc = FDIR + file
 
-file = 'h111213v/h11v12/VNP46A2.A2019242.h11v12.001.boat.hdf'
+fileExtension = ".tif"
 
+## Open HDF file (first if multiple files are in same directory)
+hdflayer = gdal.Open(file_loc, gdal.GA_ReadOnly)
 
+# Open raster layer
+for i in range(4):
+    rlayer = gdal.Open(hdflayer.GetSubDatasets()[i][0], gdal.GA_ReadOnly)
+    outputName = rlayer.GetMetadata_Dict()['long_name']
+
+    outputName = outputName.strip().replace(" ","_").replace("/","_")
+    
+    outputFolder = "figures/"
+    outputRaster = outputFolder + outputName + '.tif'
+    gdal.Warp(outputRaster, rlayer)
+    
+    
